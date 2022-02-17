2d495ba8ea703a5964a6278553ffb1172142e295
Aikowka <2556244a>
2022-02-17 21:04:08 +0600

    1

---
+++ main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	square "golang-united-school-homework-2"
+)
+
+func main() {
+	fmt.Println(square.CalcSquare(4, 0))
+	fmt.Println(square.CalcSquare(4, 3))
+	fmt.Println(square.CalcSquare(4, 4))
+	fmt.Println(square.CalcSquare(4, 5))
+}

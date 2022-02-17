9e185e0e5225015ac7c3a94748fa72b94fd5f8db
Aikowka <2556244a>
2022-02-17 21:15:02 +0600

    package

--- main/main.go
+++ main/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"golang-united-school-homework-2/square"
+	"golang-united-school-homework-2"
 )
 
 func main() {
-	fmt.Println(square.CalcSquare(4, 0))
-	fmt.Println(square.CalcSquare(4, 3))
-	fmt.Println(square.CalcSquare(4, 4))
-	fmt.Println(square.CalcSquare(4, 5))
+	fmt.Println(golang_united_school_homework_2.CalcSquare(4, 0))
+	fmt.Println(golang_united_school_homework_2.CalcSquare(4, 3))
+	fmt.Println(golang_united_school_homework_2.CalcSquare(4, 4))
+	fmt.Println(golang_united_school_homework_2.CalcSquare(4, 5))
 }
